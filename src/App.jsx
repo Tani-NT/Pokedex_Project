532bbc7163cf640ecdbef9550790a22a17dd60d0
Tani-NT <tanyanithale@gmail.com>
2024-01-20 14:38:17 +0530

    pokedex card created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
-import Pokedex from './components/Pokedex/Pokedex';
 import './App.css'
+import { Link } from 'react-router-dom';
+import CustomRoutes from './routes/CustomRoutes';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <Pokedex/>
+    <div className="outer-pokedex">
+      <h1 id="pokedex-heading">
+        <Link to="/">Pokedex</Link>
+      </h1>
+      <CustomRoutes/>
+    </div>
   )
 }
 
